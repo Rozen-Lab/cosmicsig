f53375c687263377ca22dc47dd5bfe88edf64ab8
jnh01 <44010028+jnh01@users.noreply.github.com>
2021-10-13 16:44:15 +0800

    Added package documentation in cosmicsig.R

---
+++ R/cosmicsig.R
@@ -0,0 +1,19 @@
+#' cosmicsig: A package of mutational signatures data from COSMIC (the Catalogue
+#' Of Somatic Mutations In Cancer) website
+#' \url{https://cancer.sanger.ac.uk/signatures/}
+#'
+#' This is a data package with 2 main package variables:
+#' \code{\link{signature}} and \code{\link{etiology}}.
+#'
+#' There are also several functions for handling COSMIC signatures:
+#'
+#' * \code{\link{get_etiology}}
+#'
+#' * \code{\link{SBS96_ID_to_SBS192_ID}}
+#'
+#' @source COSMIC mutational signatures data were downloaded from
+#' \url{https://cancer.sanger.ac.uk/signatures/downloads/}.
+#'
+#' @docType package
+#' @name cosmicsig
+NULL
