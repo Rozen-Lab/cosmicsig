abc7da4076a6265278db5feb81e46d10a8248583
jnh01 <44010028+jnh01@users.noreply.github.com>
2021-10-19 19:47:22 +0800

    Minor documentation updates

--- R/COSMIC_v3.0.R
+++ R/COSMIC_v3.0.R
@@ -27,7 +27,7 @@
 #'   install.packages("ICAMS")
 #' }
 #' library(ICAMS)
-#' SBS96_sigs_GRCh38_genome <- signature$SBS96
+#' SBS96_sigs_GRCh38_genome <- COSMIC_v3.0$signature$SBS96
 #'
 #' # Transform SBS96 GRCh38 genome signatures to GRCh38 exome signatures
 #' SBS96_sigs_GRCh38_exome <- TransformCatalog(catalog = SBS96_sigs_GRCh38_genome,
