80692e2225c2d4be704d6afe2f4d7d3b95676036
jnh01 <44010028+jnh01@users.noreply.github.com>
2023-08-24 15:29:23 +0800

    Change in documentation due to roxygen2 package version increase

--- R/signature.R
+++ R/signature.R
@@ -5,21 +5,21 @@
 #'
 #' @format A list with the following elements:
 #'
-#'     * GRCh37: Homo sapiens (human) genome assembly GRCh37.
-#'     * GRCh38: Homo sapiens (human) genome assembly GRCh38.
-#'     * mm9: Mus musculus (house mouse) genome assembly mm9.
-#'     * mm10: Mus musculus (house mouse) genome assembly mm10.
-#'     * rn6: Rattus norvegicus (Norway rat) genome assembly rn6.
+#' * GRCh37: Homo sapiens (human) genome assembly GRCh37.
+#' * GRCh38: Homo sapiens (human) genome assembly GRCh38.
+#' * mm9: Mus musculus (house mouse) genome assembly mm9.
+#' * mm10: Mus musculus (house mouse) genome assembly mm10.
+#' * rn6: Rattus norvegicus (Norway rat) genome assembly rn6.
 #'
-#'     Each element contains the sub elements:
+#' Each element contains the sub elements:
 #'
-#'     * SBS96: Strand-agnostic single-base substitutions in trinucleotide context.
-#'     * DBS78: Strand-agnostic doublet-base substitutions.
+#' * SBS96: Strand-agnostic single-base substitutions in trinucleotide context.
+#' * DBS78: Strand-agnostic doublet-base substitutions.
 #'
-#'     Element GRCh37 contains the additional sub elements:
+#' Element GRCh37 contains the additional sub elements:
 #'
-#'     * SBS192: Transcriptionally stranded single-base substitutions in trinucleotide context.
-#'     * ID: Strand-agnostic indels (short insertions and deletions).
+#' * SBS192: Transcriptionally stranded single-base substitutions in trinucleotide context.
+#' * ID: Strand-agnostic indels (short insertions and deletions).
 #'
 #' @section Remark:
 #' The signatures are all genome signatures. \cr See \link[ICAMS]{CatalogRowOrder}
